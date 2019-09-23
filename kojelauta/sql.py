cfc328d5b2fe560ff2c40accf43809cc370d03c2
gitLari <mrlari@hotmail.com>
2019-09-23 11:38:45 +0000

    vm koneen kamat ohjeena alussa

--- kojelauta/sql.py
+++ kojelauta/sql.py
@@ -1,8 +1,23 @@
 # -*- coding: latin-1 -*-
 #!/usr/bin/python
-#connector tarvitaan pythoniin, jotta toimii -alla 
-# sudo apt install python3-pip
-# pip install mysqlclient
+#
+""" nämä tarpeen virtuaalikoneessa. python 2 tässä ihan vaan
+sudo apt-get update
+sudo apt-get install -y git
+sudo apt-get install -y docker.io
+sudo docker build -t mariadb .
+sudo docker run -d --name mariadb mariadb
+sudo apt-get install -y python
+sudo apt install -y python-pip
+sudo apt-get install -y nano
+pip install mysql-connector
+git clone https://github.com/TiinaMaar/Kojelauta.git
+cd Kojelauta
+git checkout sql
+cd kojelauta
+python sql.py//
+"""
+
 import mysql.connector as mariadb
 
 mariadb_connection = mariadb.connect(host='172.17.0.2', port='3306',user='tballs', password='big', database='tballs')
